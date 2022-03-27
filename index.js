3ddd8466513fb4384f096265d7338c6a3c0622af
yang2674 <steve@softwareforgood.com>
2022-03-27 13:31:02 -0500

    add background-images, sounds to each key

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 1. addEventListener is like waiting for a notification from someone
 2. anonymous functions don't have a name, and can still execute code
 3. higher-order functions: passing in a function into a method [i.e., .addEventListener('click', handleClick)]
+4. var audio = new Audio('url'); audio.play();
+5. 'this' keyword gives us the identity of the object; this is really useful because we can change the style.color of the object by calling this
 
 
 /*obstacles:
@@ -12,12 +14,48 @@
 5. forgot ; in for loop
 */
 
-//this allows each button to display 'I got clicked!' message when clicked
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i<numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "a":
+                var kickBass = new Audio('sounds/kick-bass.mp3');
+                kickBass.play();
+                break;
+            case "s":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case "d":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "l":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            default:
+                break;
+        }
     });
 }
-
